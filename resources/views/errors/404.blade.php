d55427967a9ea48a809928637ddeebf8c9c78e2a
shenyazi <1047340212@qq.com>
2017-12-05 22:05:15 +0800

    syz

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <link rel="shortcut icon" href="{{asset('admins/img/favicon.html')}}">
+
+    <title>404</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('admins/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('admins/css/bootstrap-reset.css')}}" rel="stylesheet">
+    <!--external css-->
+    <link href="{{asset('admins/assets/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="{{asset('admins/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('admins/css/style-responsive.css')}}" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+  <body class="body-404">
+
+    <div class="container">
+
+      <section class="error-wrapper">
+          <i class="icon-404"></i>
+          <h1>404</h1>
+          <h2>网页开小差,飞到月球约会去了.....</h2>
+          <p class="page-404">Something went wrong or that page doesn’t exist yet. <a href="{{url('/admin')}}">Return Home</a></p>
+      </section>
+
+    </div>
+
+
+  </body>
+</html>
