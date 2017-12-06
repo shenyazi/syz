909bd871c306602262c1d37700faae7c9e6e4d41
wyh <you@example.com>
2017-12-06 09:11:07 +0800

    修改

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -127,30 +127,8 @@
                       </ul>
                   </li>
 
-                  <li class="sub-menu">
-                      <a href="javascript:;" class="">
-                          <i class="icon-cogs"></i>
-                          <span>Components</span>
-                          <span class="arrow"></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a class="" href="grids.html">Grids</a></li>
-                          <li><a class="" href="calendar.html">Calendar</a></li>
-                          <li><a class="" href="charts.html">Charts</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" class="">
-                          <i class="icon-tasks"></i>
-                          <span>Form Stuff</span>
-                          <span class="arrow"></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a class="" href="form_component.html">Form Components</a></li>
-                          <li><a class="" href="form_wizard.html">Form Wizard</a></li>
-                          <li><a class="" href="form_validation.html">Form Validation</a></li>
-                      </ul>
-                  </li>
+                  
+                  
                   <li class="sub-menu">
                       <a href="javascript:;" class="">
                           <i class="icon-th"></i>
@@ -162,33 +140,9 @@
                           <li><a class="" href="dynamic_table.html">浏览商品</a></li>
                       </ul>
                   </li>
-                  <li>
-                      <a class="" href="inbox.html">
-                          <i class="icon-envelope"></i>
-                          <span>Mail </span>
-                          <span class="label label-danger pull-right mail-info">2</span>
-                      </a>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" class="">
-                          <i class="icon-glass"></i>
-                          <span>Extra</span>
-                          <span class="arrow"></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a class="" href="blank.html">Blank Page</a></li>
-                          <li><a class="" href="profile.html">Profile</a></li>
-                          <li><a class="" href="invoice.html">Invoice</a></li>
-                          <li><a class="" href="404.html">404 Error</a></li>
-                          <li><a class="" href="500.html">500 Error</a></li>
-                      </ul>
-                  </li>
-                  <li>
-                      <a class="" href="login.html">
-                          <i class="icon-user"></i>
-                          <span>Login Page</span>
-                      </a>
-                  </li>
+                 
+                 
+                  
 				         </ul>
               <!-- sidebar menu end-->
           </div>
